782b482c4a3bcb2978ca6ae51f815c1303de4ce9
Carlos Dochao <cadochao@ucm.es>
2025-03-14 15:41:13 +0100

    Apañao

--- Scripts/virtual_cursor.gd
+++ Scripts/virtual_cursor.gd
@@ -1,13 +1,8 @@
 extends Node2D
 @export var camera: Camera2D
 
-<<<<<<< Updated upstream
-func _process(delta) -> void:
-	
-=======
 func _init() -> void:
 	SignalManager.InitializeCursor(self.position)
 
 func _process(delta: float) -> void:
->>>>>>> Stashed changes
 	position = camera.get_global_mouse_position()
