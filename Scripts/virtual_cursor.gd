0a05e6e80ddb212f2ae591bee803231ffd0bd84d
Carlos Dochao <cadochao@ucm.es>
2025-03-14 14:09:23 +0100

    Pieza transformada en icon

--- Scripts/virtual_cursor.gd
+++ Scripts/virtual_cursor.gd
@@ -2,6 +2,6 @@ extends Node2D
 var cursorPosition
 @export var camera: Camera2D
 
-func _process(delta: float) -> void:
+func _process(delta) -> void:
 	
 	position = camera.get_global_mouse_position()
