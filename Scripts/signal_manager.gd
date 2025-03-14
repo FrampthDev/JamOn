4710f920bdc2a5e870a5ee4b663fca6f8a412d34
link10804k <62154757+link10804k@users.noreply.github.com>
2025-03-14 21:20:00 +0100

    Las fichas me van a matar

--- Scripts/signal_manager.gd
+++ Scripts/signal_manager.gd
@@ -3,20 +3,32 @@ extends Node2D
 signal PieceEnter
 signal PieceExit
 
+signal SquareEnter
+signal SquareExit
+
 var cursor
+var click: bool = false
+var dragged: bool = false
+
 var buffer: Array
 var bufferIndex: int = 0
-var click: bool = false
+
+var squareBuffer: Array
+var squareBufferIndex: int = 0
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("Click"):
-		click = true
+		click = true	
 	else: if event.is_action_released("Click"):
+		if dragged:
+			buffer[0].position = squareBuffer[0].global_position
 		click = false
+		dragged = false
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if bufferIndex > 0 && click:
 		buffer[0].position = cursor.position
+		dragged = true
 
 func InitializeCursor(_cursor: Node2D) -> void:
 	cursor = _cursor
@@ -24,7 +36,6 @@ func InitializeCursor(_cursor: Node2D) -> void:
 func _on_piece_enter(piece: Node2D) -> void:
 	buffer.append(piece)
 	bufferIndex += 1
-	print("entrar")
 
 func _on_piece_exit(piece: Node2D) -> void:
 	var i: int = 0
@@ -33,8 +44,25 @@ func _on_piece_exit(piece: Node2D) -> void:
 	if i < bufferIndex - 1: 
 		moveLeft(i)
 	bufferIndex -= 1
-	print("salir")
 
 func moveLeft(i: int) -> void:
-	for j in (bufferIndex - i):
+	for j in (bufferIndex - i - 1):
 		buffer[i + j] = buffer[i + j + 1]
+
+func _on_square_enter(piece: Node2D) -> void:
+	squareBuffer.append(piece)
+	squareBufferIndex += 1
+	print(squareBufferIndex)
+
+func _on_square_exit(piece: Node2D) -> void:
+	var i: int = 0
+	while i < squareBufferIndex && piece != squareBuffer[i]:
+		i += 1
+	if i < squareBufferIndex - 1: 
+		moveLeftBuffer(i)
+	squareBufferIndex -= 1
+	print(squareBufferIndex)
+
+func moveLeftBuffer(i: int) -> void:
+	for j in (squareBufferIndex - i - 1):
+		squareBuffer[i + j] = squareBuffer[i + j + 1]
--- Scripts/square.gd
+++ Scripts/square.gd
@@ -2,3 +2,12 @@ extends Node2D
 
 var coor: int
 var occupied: bool
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.name == "AreaPlacement":
+		SignalManager.SquareEnter.emit(self)
+
+
+func _on_area_2d_area_exited(area: Area2D) -> void:
+	if area.name == "AreaPlacement":
+		SignalManager.SquareExit.emit(self)
