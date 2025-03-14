a79382ad3f7c0593f3730fdce42b079fab7f6982
Frampth <frampth@proton.me>
2025-03-14 13:39:57 +0100

    mongas

--- Scripts/virtual_cursor.gd
+++ Scripts/virtual_cursor.gd
@@ -2,9 +2,6 @@ extends Node2D
 var cursorPosition
 @export var camera: Camera2D
 
-func _init() -> void:
-	cursorPosition = $SubViewport
-
 func _process(delta: float) -> void:
 	
 	position = camera.get_global_mouse_position()
