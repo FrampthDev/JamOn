7fdaad02b6dc9e28de5e4700e41752a9231392b5
link10804k <62154757+link10804k@users.noreply.github.com>
2025-03-15 11:36:20 +0100

    cositas (falta poder meter más de una ficha)

--- Scripts/piece.gd
+++ Scripts/piece.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+var leftGen: Node2D
+var rightGen: Node2D
+var flipX: bool
+
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.name == "AreaMouse":
 		SignalManager.PieceEnter.emit(self)
@@ -7,3 +11,18 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 func _on_area_2d_area_exited(area: Area2D) -> void:
 	if area.name == "AreaMouse":
 		SignalManager.PieceExit.emit(self)
+
+func _init() -> void:
+	#leftGen = (gen)
+	#rightGen = (gen)
+	pass
+
+func switchGens() -> void:
+	var temp = leftGen
+	leftGen = rightGen
+	rightGen = temp
+	
+	flipX = !flipX
+	$Icon.set_flip_h(flipX)
+	
+	print("piece flipped ", "flipX ", flipX)
